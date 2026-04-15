df834fd4b4fecae1a1a7baea3f316391de8e5463
madisvoitk <mvoitk@taltech.ee>
2026-04-15 10:20:56 +0300

    fix

--- JS/vue_front_to_PP/vite.config.ts
+++ JS/vue_front_to_PP/vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mvoitk-cs3.proxy.itcollege.ee',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })

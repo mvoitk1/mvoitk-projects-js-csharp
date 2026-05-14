3daf33deb1b7f8644b8e4da141906c4b5235ad4b
madisvoitk <mvoitk@taltech.ee>
2026-05-14 23:00:52 +0300

    homecontroller fixed and 6 views updated

--- Csharp/Assignment_3/WebApp/Controllers/HomeController.cs
+++ Csharp/Assignment_3/WebApp/Controllers/HomeController.cs
@@ -2,14 +2,10 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using App.DAL.EF;
-using App.DTO.v1.Categories;
-using App.DTO.v1.Collections;
-using App.DTO.v1.Products;
+using App.BLL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApp.ViewModels;
 
@@ -18,58 +14,27 @@ namespace WebApp.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly AppDbContext _context;
+    private readonly ICollectionService _collectionService;
+    private readonly ICategoryService _categoryService;
+    private readonly IProductService _productService;
 
-    public HomeController(AppDbContext context, ILogger<HomeController> logger)
+    public HomeController(
+        ICollectionService collectionService,
+        ICategoryService categoryService,
+        IProductService productService,
+        ILogger<HomeController> logger)
     {
         _logger = logger;
-        _context = context;
+        _collectionService = collectionService;
+        _categoryService = categoryService;
+        _productService = productService;
     }
 
     public async Task<IActionResult> Index()
     {
-        var collections = await _context.Collections
-            .Where(c => c.IsActive)
-            .OrderByDescending(c => c.LaunchDate)
-            .Take(3)
-            .Select(c => new CollectionDto
-            {
-                Id = c.Id,
-                Name = c.Name.Translate()!,
-                Description = c.Description.Translate()!,
-                LaunchDate = c.LaunchDate,
-                IsActive = c.IsActive
-            })
-            .ToListAsync();
-
-        var categories = await _context.Categories
-            .Where(c => c.ParentCategoryId == null)
-            .Take(4)
-            .Select(c => new CategoryDto
-            {
-                Id = c.Id,
-                Name = c.Name.Translate()!,
-                ParentCategoryId = c.ParentCategoryId
-            })
-            .ToListAsync();
-
-        var products = await _context.Products
-            .Where(p => p.IsActive)
-            .Include(p => p.Variants)
-            .Include(p => p.Images)
-            .OrderByDescending(p => p.CreatedAt)
-            .Take(6)
-            .ToListAsync();
-
-        var featuredProducts = products.Select(p => new ProductListItemDto
-        {
-            Id = p.Id,
-            Name = p.Name.Translate()!,
-            Gender = p.Gender.ToString(),
-            IsActive = p.IsActive,
-            LowestPrice = p.Variants.Any() ? p.Variants.Min(v => v.Price) : null,
-            PrimaryImageUrl = p.Images.OrderBy(i => i.SortOrder).FirstOrDefault()?.Url
-        }).ToList();
+        var collections = (await _collectionService.GetActiveAsync()).Take(3).ToList();
+        var categories = (await _categoryService.GetAllAsync()).Take(4).ToList();
+        var featuredProducts = (await _productService.GetListAsync()).Take(6).ToList();
 
         return View(new HomeViewModel
         {

1edf8f65e3e7ee044d735c2978022285c549e038
madisvoitk <mvoitk@taltech.ee>
2026-05-16 00:15:31 +0300

    type mismatch bug fix

--- Csharp/Assignment_3/WebApp/Controllers/OrdersController.cs
+++ Csharp/Assignment_3/WebApp/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 using App.BLL.Contracts;
+using App.DTO.Mappers;
+using App.DTO.v1.Orders;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Helpers;
@@ -11,13 +13,13 @@ public class OrdersController(IOrderService orderService) : Controller
     public async Task<IActionResult> Index()
     {
         var orders = await orderService.GetUserOrdersAsync(User.UserId());
-        return View(orders);
+        return View(orders.MapList<OrderListItemDto>());
     }
 
     public async Task<IActionResult> Detail(Guid id)
     {
         var order = await orderService.GetUserOrderByIdAsync(User.UserId(), id);
         if (order == null) return NotFound();
-        return View(order);
+        return View(order.MapTo<OrderDto>());
     }
 }
